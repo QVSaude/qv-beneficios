e840bc42abad6ff5209d6aa8c31af933fc318655
AndersonMoura <andersonmoura812@gmail.com>
2025-05-07 11:55:25 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });*/
+
+  var $doc = $('html, body');
+$('a').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
